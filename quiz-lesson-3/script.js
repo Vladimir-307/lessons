234d2047a5606c63ef0aa3c42add7413ec99317f
Vladimir <Barinov.V.Y@sberbank.ru>
2021-03-30 21:23:22 +0400

    quiz-lesson-3

--- quiz-lesson-3/script.js
+++ quiz-lesson-3/script.js
@@ -1,2 +1,9 @@
-let now = new Date('03/13/2020');
-console.log(now === "Invalid Date");
+// Вопрос 1
+let date1 = new Date(2021, 2, 30, 0, 0, 0, 0);
+let date2 = new Date(2021, 2, 30, 17, 20, 0, 0);
+
+console.log(date2-date1);
+
+// Вопрос 4
+let now = new Date(1958,8,2);
+console.log(now.getDay());
